edf20bf2464587713656844496a0e4677fd03569
Chris West (Faux) <git@goeswhere.com>
2024-06-30 18:04:11 +0100

    feat: sprite sheet

--- scripts/sprite-sheet.ts
+++ scripts/sprite-sheet.ts
@@ -1,47 +1,94 @@
 #!/usr/bin/env tsx
 
 import spritesmith from 'spritesmith';
-import { mkdirSync, readFileSync } from 'fs';
-import { JRecipe } from '@/lib/j-types';
-import { existsSync } from 'node:fs';
-import { mkdtemp } from 'node:fs/promises';
+import { readFileSync } from 'fs';
+import { JCraftable, JRecipe } from '@/lib/j-types';
+import { existsSync, writeFileSync } from 'node:fs';
+import { mkdtemp, rm } from 'node:fs/promises';
 import sharp from 'sharp';
+import { createHash } from 'node:crypto';
 
 async function main() {
   const root = 'raw-script-output';
-  const wanted: string[] = [];
-  const recps: JRecipe[] = JSON.parse(
-    readFileSync('data/recipes.json', 'utf-8'),
-  );
+  type Path = string;
+  type Colon = ['craft' | 'recipe' | 'tech', string];
+  const wanted: Record<string, Colon[]> = {};
+
+  const addWanted = (path: Path, type: Colon) => {
+    if (!wanted[path]) wanted[path] = [];
+    wanted[path].push(type);
+  };
+
   const exists = (name: string) => existsSync(`${root}/${name}.png`);
-  for (const r of recps) {
-    if (exists(`recipe/${r.id}`)) wanted.push(`recipe/${r.id}.png`);
+
+  for (const r of JSON.parse(
+    readFileSync('data/recipes.json', 'utf-8'),
+  ) as JRecipe[]) {
+    const guess = `recipe/${r.id}`;
+    if (exists(guess)) addWanted(guess, ['recipe', r.id]);
     else throw new Error(`Missing recipe image: ${r.id}`);
   }
 
-  const tmp = await mkdtemp('sprites-');
-  mkdirSync(`${tmp}/recipe`, { recursive: true });
-  const src: string[] = [];
-  for (const w of wanted) {
-    const img = sharp(`${root}/${w}`);
-    const fileOut = `${tmp}/${w}`;
-    await img.resize(32).toFile(fileOut);
-    src.push(fileOut);
+  outer: for (const r of JSON.parse(
+    readFileSync('data/craftables.json', 'utf-8'),
+  ) as JCraftable[]) {
+    for (const cat of ['fluid', 'item', 'entity']) {
+      const guess = `${cat}/${r.id}`;
+      if (exists(guess)) {
+        addWanted(guess, ['craft', r.id]);
+        continue outer;
+      }
+    }
+    console.log(`Missing craftable image: ${r.id}`);
   }
+
+  const tmp = await mkdtemp('sprites-');
+
+  const pathHashes: Record<string, string[]> = {};
+  for (const [path, types] of Object.entries(wanted)) {
+    const hash = createHash('sha512');
+    const buf = await sharp(`${root}/${path}.png`).resize(32).toBuffer();
+    hash.update(buf);
+    const digest = hash.digest('hex').slice(0, 32);
+    if (!pathHashes[digest]) {
+      pathHashes[digest] = [];
+      writeFileSync(`${tmp}/${digest}.png`, buf);
+    }
+    pathHashes[digest].push(path);
+  }
+
+  const src = Object.keys(pathHashes).map((digest) => `${tmp}/${digest}.png`);
 
   const result = await new Promise<spritesmith.SpritesmithResult>(
     (resolve, reject) =>
       spritesmith.run(
         {
           src,
+          exportOpts: {
+            format: 'png',
+          },
         },
         (err, result) => (err ? reject(err) : resolve(result)),
       ),
   );
 
-  await sharp(result.image).toFile('data/recipe-sheet.webp');
+  await sharp(result.image).toFile('data/icons.webp');
 
-  console.log(result);
+  await rm(tmp, { recursive: true, force: true });
+
+  const out: Record<string, [number, number]> = {};
+
+  for (const [fakePath, props] of Object.entries(result.coordinates)) {
+    const fakePng = fakePath.substring(tmp.length + 1);
+    const hash = fakePng.slice(0, fakePng.length - 4);
+    for (const realPath of pathHashes[hash]) {
+      for (const colon of wanted[realPath]) {
+        out[`${colon[0]}:${colon[1]}`] = [props.x, props.y];
+      }
+    }
+  }
+
+  writeFileSync('data/icons.json', JSON.stringify(out));
 }
 
 main().catch((e) => {
